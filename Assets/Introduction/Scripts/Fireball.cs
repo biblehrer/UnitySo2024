f824e1c1dc3b0be6ef8207b52cf6147fd910fcaa
Stefan Gerstner <stefan.gerstner96@web.de>
2024-04-18 11:50:35 +0200

    Fireball Rotation

--- Assets/Introduction/Scripts/Fireball.cs
+++ Assets/Introduction/Scripts/Fireball.cs
@@ -1,20 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Fireball : MonoBehaviour
 {
+    public Vector3 direction = Vector3.up;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, 3);
+
+        // Rotate the fireball 
+        float angle = Vector3.Angle(Vector3.right, direction);
+
+        if (direction.y < 0)
+        {
+            angle = angle*-1;
+        }
+
+        transform.Rotate(new Vector3(0,0,angle));
+
+        /*
+        if (direction.y > 0)
+        {
+            transform.Rotate(new Vector3(0,0,90));
+        }
+        if (direction.y < 0)
+        {
+            transform.Rotate(new Vector3(0,0,-90));
+        }
+        if (direction.x < 0)
+        {
+            transform.Rotate(new Vector3(0,0,180));
+        }*/
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + new Vector3(0,1,0) * Time.deltaTime * 4;
-
-        //Destroy(gameObject);
+        transform.position = transform.position + direction * Time.deltaTime * 4;
     }
 }
--- Assets/Introduction/Scripts/Wizard.cs
+++ Assets/Introduction/Scripts/Wizard.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class Wizard : MonoBehaviour
 {
     public GameObject fireballPrefab;
+    private float counter = 0;
+    private Vector3 lastMovement = Vector3.zero;
 
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(fireballPrefab, transform.position, Quaternion.identity);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Movement
         Vector3 movement = new Vector3(0,0,0);
         if (Input.GetKey("w"))
         {
@@ -36,5 +39,23 @@ public class Wizard : MonoBehaviour
         movement = movement.normalized;
 
         transform.position = transform.position + movement * Time.deltaTime * 2;
+
+        if (movement.y != 0 || movement.x != 0)
+        {
+            lastMovement=movement;
+        }
+        
+
+        //GetKomponent<Fireball>().
+        // Casting
+        counter += Time.deltaTime;
+        if (counter > 2 && Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject obj = Instantiate(fireballPrefab, transform.position, Quaternion.identity);
+            obj.GetComponent<Fireball>().direction = lastMovement;
+            counter = 0;
+        }
+
+        
     }
 }
